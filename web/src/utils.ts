bf4142e2081e372ccd7ada2caffc11934400e3cb
Mariano Strugo <itwarp_mstrugo@iseatz.com>
2021-06-07 12:27:42 -0300

    Set production GraphQL endpoint in the React app

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,3 +1,8 @@
+export const GRAPHQL_PROD_URL = 'https://t9-phonewords-server.herokuapp.com';
+export const GRAPHQL_LOCAL_URL = 'http://localhost:4000';
+
+export const getGraphUrl = () => (process.env.NODE_ENV === 'production' ? GRAPHQL_PROD_URL : GRAPHQL_LOCAL_URL);
+
 export const dialerLetters = [undefined, 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ', undefined];
 
 export const dialerNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -11,7 +16,7 @@ export const getPhonewords = async (term: string) => {
     }
   `;
 
-  const response = await fetch('http://localhost:4000/graphql', {
+  const response = await fetch(`${getGraphUrl()}/graphql`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ query }),
